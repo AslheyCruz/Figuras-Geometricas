7a9429c48f2365df602befd83d93eed2cc1b82fb
AslheyCruz <62490538+AslheyCruz@users.noreply.github.com>
2020-04-24 10:46:49 -0500

    Delete Prueba.py

--- Prueba.py
+++
@@ -1,8 +0,0 @@
-calf1 = int(input("dijite la primera calificacion: "))
-calf2 = int(input("dijite la segunda calificacion: "))
-resultado = calf1 + calf2 /2
-
-if resultado > 60:
-    print("Alumno aprobado")
-else:
-    print("Alumno reprobado")
